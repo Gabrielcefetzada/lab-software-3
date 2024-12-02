b62acf726e9151f45f64edc4b5968f96d51c99ba
Gabrielcefetzada <gabrielaugustosb13@gmail.com>
2024-12-02 13:33:04 -0300

    feat: add userId on login responde

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -11,12 +11,13 @@ export class AuthService {
   async signIn(
     email: string,
     password: string,
-  ): Promise<{ access_token: string }> {
+  ): Promise<{ access_token: string, userId: number }> {
     const user = await this.userService.findUserByEmail(email);
     if (!user || user.password !== password) throw new UnauthorizedException();
     const payload = { sub: user.id, username: user.email};
     return {
       access_token: await this.jwtService.signAsync(payload),
+      userId: user.id
     };
   }
 
